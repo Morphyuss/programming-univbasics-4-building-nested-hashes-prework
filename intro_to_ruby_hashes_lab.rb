9c9405c35f649b867ce3664b749a6630eae788c3
James Stamps Jr <jstampshere@gmail.com>
2020-04-17 16:35:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars.has_key?(:one_piece_owned) => {},
+      :rent_in_dollars => {},
       :names => {}
     }
   }
